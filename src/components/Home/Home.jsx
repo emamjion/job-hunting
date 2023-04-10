de68bc25077e55f0d5978e26d0dbe7b43ca1add0
emamjion <emamjion@gmail.com>
2023-04-10 15:47:48 +0600

    job category section design finished

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import bannerImg from '../../assets/All Images/P3OLGJ1 copy 1.png';
+import accountImg from '../../assets/Icons/accounts 1.png';
+import businessImg from '../../assets/Icons/business 1.png';
+import marketingImg from '../../assets/Icons/social-media 1.png';
+import engineeringImg from '../../assets/Icons/chip 1.png';
 
 const Home = () => {
     return (
@@ -18,6 +22,37 @@ const Home = () => {
                 {/* banner - image side */}
                 <div>
                     <img src={bannerImg} alt="" />
+                </div>
+            </section>
+            {/* Job Category List section */}
+            <section className='px-72 mt-16 '>
+                <div className='text-center mb-8'>
+                    <h1 className='text-5xl font-medium mb-5'>Job Category List</h1>
+                    <p className='text-[#757575]'>
+                        Explore thousands of job opportunities with all the information you need. Its your future
+                    </p>
+                </div>
+                <div className='flex items-center justify-center gap-6 mb-12'>
+                    <div className='bg-[#F9F9FF] p-10 rounded-lg'>
+                        <img className='bg-[#EFECFF] p-2 rounded' src={accountImg} alt="" />
+                        <h3 className='text-xl font-medium mt-8 mb-2'>Account & Finance</h3>
+                        <p className='text-[#A3A3A3]'>300 Jobs Available</p>
+                    </div>
+                    <div className='bg-[#F9F9FF] p-10 rounded-lg'>
+                        <img className='bg-[#EFECFF] p-2 rounded' src={businessImg} alt="" />
+                        <h3 className='text-xl font-medium mt-8 mb-2'>Creative Design</h3>
+                        <p className='text-[#A3A3A3]'>100+ Jobs Available</p>
+                    </div>
+                    <div className='p-10 bg-[#F9F9FF] rounded-lg'>
+                        <img className='bg-[#EFECFF] p-2 rounded' src={marketingImg} alt="" />
+                        <h3 className='text-xl font-medium mt-8 mb-2'>Marketing & Sales</h3>
+                        <p className='text-[#A3A3A3]'>150 Jobs Available</p>
+                    </div>
+                    <div className='bg-[#F9F9FF] p-10 rounded-lg'>
+                        <img className='bg-[#EFECFF] p-2 rounded' src={engineeringImg} alt="" />
+                        <h3 className='text-xl font-medium mt-8 mb-2'>Engineering Job</h3>
+                        <p className='text-[#A3A3A3]'>224 Jobs Available</p>
+                    </div>
                 </div>
             </section>
         </div>
